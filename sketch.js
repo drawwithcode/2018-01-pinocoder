2a9879e45f7ec39508e0b3a3436c745459781f6a
pinocoder <perico.jacopo@gmail.com>
2018-09-26 02:57:27 +0200

    Final version

--- sketch.js
+++ sketch.js
@@ -2,22 +2,49 @@ function setup() {
 	// define artboard size
 	createCanvas(500,500);
 	// Set here the values that are not changing all over the sketch
-	strokeWeight(2);
-	stroke(170);
+	//strokeWeight(2);
+	//stroke(170);
 }
 
 function draw() {
-	background(204);
+	background(247, 247, 247);
+	//Head
+	fill(255, 255, 255);
+	stroke(0, 0, 0);
+	rect(150, 80, 140, 90);
 
-	//Neck
-	line(400, 285, 250, 345);
 
-	//Wheel
-	ellipse(600, 465, 70, 70);
+
+	//Eyes
+	fill(0, 0, 0);
+	rect(160, 120, 120, 30);
+	fill(255, 255, 255);
+	ellipse(190, 135, 20);
+	ellipse(250, 135, 20);
+
 
 	//Body
-	rect(50, 325, 100, 140);
+	fill(255, 255, 255);
+	stroke(0, 0, 0);
+	triangle(220, 230, 250, 300, 190, 300);
+	triangle(250, 300, 270, 230, 220, 230);
+	triangle(190, 300, 220, 230, 170, 230);
+	rect(170, 170, 100, 60);
+	fill(255, 224, 024);
+	noStroke();
+	text("PLAY ME!", 190, 200);
+	//Feet
+	fill(0,0,0);
+	stroke(255, 255, 255);
+	triangle(250, 300, 220, 350, 280, 350);
+	triangle(220, 350, 250, 300, 190, 300);
+	triangle(190, 300, 220, 350, 160, 350);
 
-	//Head
-	ellipse(250, 240, 90, 90);
+	//Arms
+	fill (255, 224, 024);
+	triangle (170, 170, 170, 200, mouseX, mouseY);
+	triangle (270, 170, 270, 200, mouseX, mouseY);
+
+
+
 }
